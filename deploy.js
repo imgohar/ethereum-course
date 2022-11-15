c48747530167209348d6f9ce8d03a3851fca353e
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2022-11-15 12:13:56 +0500

    test

--- deploy.js
+++ deploy.js
@@ -17,7 +17,7 @@ const deploy = async () => {
     console.log(accounts[0]);
 
     const result = await new web3.eth.Contract(interface)
-        .deploy({ data: bytecode, arguments: ['Hi, There'] })
+        .deploy({ data: bytecode })
         .send({ gas: 1000000, from: accounts[0] });
 
     console.log(result);
