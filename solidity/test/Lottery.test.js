b8e0da9c620bf91e4bfbb725d8978cae348d91e8
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2022-11-16 09:12:37 +0500

    Folder structure

--- solidity/test/Lottery.test.js
+++ solidity/test/Lottery.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const compile = require('../compile');
+const compile = require('../../compile');
 
 const interface = compile.abi;
 const bytecode = compile.evm.bytecode.object;
